453b24b2629d367bf301b8fffc66debfe4715325
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-10-25 16:21:20 +0530

    added cors

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ import express from 'express';
 import userRoutes from './src/routes/user.js';
 import movieRoutes from './src/routes/movie.js';
 import articleRoutes from './src/routes/article.js';
+import cors from 'cors';
+import compression from 'compression'
 dotenv.config();
 
 const app = express();
 
+const corsConfig = {
+    credentials: "true",
+    origin: "http://localhost:3000",
+    optionSuccessStatus: "200",
+};
+
+app.use(compression());
+app.use(cors(corsConfig));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
@@ -18,7 +28,7 @@ app.use('/article', articleRoutes);
 
 const PORT = process.env.PORT || 3300;
 mongoose.connect(process.env.MONGO_URL)
-    .then(()=>app.listen(PORT, ()=> {
+    .then(() => app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     }))
-    .catch((err)=>console.log(err))
+    .catch((err) => console.log(err))
