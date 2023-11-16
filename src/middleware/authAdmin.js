29c0df19cefad6f4d11c4ef4ebd5319edd0ae65c
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-11-17 00:32:07 +0530

    fixed logic

--- src/middleware/authAdmin.js
+++ src/middleware/authAdmin.js
@@ -5,13 +5,16 @@ export const authAdmin = async (req, res, next) => {
   try {
     const authorization = req.headers.authorization;
     const password = req.headers.password;
+    if (!authorization || !password)
+      return res.status(401).json({ message: "Unauthorised Access." });
     if (
       authorization !== process.env.AUTHORIZATION ||
       password !== process.env.PASSWORD
     )
       return res.status(401).json({ message: "Unauthorised Access." });
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    return res.status(500).json({ message: "Internal Server Error." });
   }
   next();
 };
